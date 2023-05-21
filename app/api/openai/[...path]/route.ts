b7fc9405213813b1122e33117b633a885d94686a
zhangxq <zhangxq@yunnancopper.com>
2023-05-21 16:32:16 +0800

    记录req.body

--- app/api/openai/[...path]/route.ts
+++ app/api/openai/[...path]/route.ts
@@ -8,6 +8,12 @@ async function handle(
   { params }: { params: { path: string[] } },
 ) {
   console.log("[OpenAI Route] params ", params);
+  const reqA = req.clone();
+  let reqBody;
+  if (reqA.body) {
+    reqBody = await reqA.json();
+  }
+  console.log("[reqBody] \n", JSON.stringify(reqBody));
 
   const authResult = auth(req);
   if (authResult.error) {
