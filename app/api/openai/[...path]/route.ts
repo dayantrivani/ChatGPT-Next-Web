1d9d76967de481478d09235f85a311bb496888cc
Zhangxq <zhangxq@yunnancopper.com>
2023-05-22 10:26:29 +0800

    log Request content

--- app/api/openai/[...path]/route.ts
+++ app/api/openai/[...path]/route.ts
@@ -9,6 +9,15 @@ async function handle(
 ) {
   console.log("[OpenAI Route] params ", params);
 
+  // log Request content
+  req.clone().json()
+    .then((reqBody: any) => {
+      console.log("[reqBody]\n", JSON.stringify(reqBody));
+    })
+    .catch((error: any) => {
+      console.error("Error occurred while parsing request body: ", error);
+    });
+
   const authResult = auth(req);
   if (authResult.error) {
     return NextResponse.json(authResult, {
